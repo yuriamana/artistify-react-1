c7acdf930a5c1a19546d133f6ee0751fab539867
Yuki Kato <katoyuki0113@gmail.com>
2021-11-08 12:50:48 +0100

    artist get done

--- server/routes/artists.js
+++ server/routes/artists.js
@@ -7,8 +7,31 @@ const router = new express.Router();
 const artistModel = require("../model/Artist");
 const albumModel = require("../model/Album");
 
-router.get("/artists", (req, res) => {
-  res.send("@todo");
+router.get("/artists", async (req, res, next) => {
+  try {
+    const artists = await artistModel.find()
+    res.status(200).json(artists);
+  } catch(e) {
+    next(e);
+  }
+})
+router.get("/artists",  async (req, res, next) => {
+  try {
+    const newArtist = await artistModel.create(req.body);
+    res.status(201).json(newArtist)
+  } catch(e) {
+    next(e)
+  }
+  // res.send("@todo");
 });
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const artist = await artistModel.findById(req.params.id);
+    res.status(200).json(artist)
+  } catch(e) {
+    next(e)
+  }
+})
 
 module.exports = router;
