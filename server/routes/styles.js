4fbe35cc381a35fdbcb7e2818f1fbf37ee7c4b45
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-11-08 15:42:10 +0100

    styles route backend

--- server/routes/styles.js
+++ server/routes/styles.js
@@ -5,8 +5,35 @@ const express = require("express");
 const router = new express.Router();
 const styleModel = require("../model/Style");
 
-router.get("/styles", (req, res) => {
-  res.send("@todo");
+// READ
+
+router.get("/styles", async (req, res, next) => {
+  try {
+    const styles = await styleModel.find()
+    res.status(200).json(styles);
+  } catch(e) {
+    next(e);
+  }
+})
+
+router.get("/styles/:id", async (req, res, next) => {
+  try {
+    const style = await styleModel.findById(req.params.id);
+    res.status(200).json(style)
+  } catch(e) {
+    next(e)
+  }
+})
+
+// DELETE
+
+router.delete("/styles/:id", async (req, res, next) => {
+  try {
+    const deletedStyles = await styleModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedStyles);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
