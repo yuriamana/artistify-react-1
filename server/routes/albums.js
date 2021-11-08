10b0b26eb2f7823b31514214e492deeba096b4fd
Joey6Hang <flamincos517@gmail.com>
2021-11-08 15:43:17 +0100

    routes albums

--- server/routes/albums.js
+++ server/routes/albums.js
@@ -8,7 +8,35 @@ const albumModel = require("../model/Album");
 const uploader = require("./../config/cloudinary");
 
 router.get("/albums", (req, res) => {
-  res.send("@todo");
+  res.send("cooucoou");
+});
+
+router.get("/albums", async (req, res, next) => {
+  console.log(json(albums))
+  try {
+    const albums = await albumModel.find();
+    res.status(200).json(albums);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/albums/:id", async (req, res, next) => {
+  try {
+    const album = await albumModel.findById(req.params.id);
+    res.status(200).json(album);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedalbum = await albumModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedalbum);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
